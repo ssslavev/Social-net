0cc6ffd7e93145bbf427a2ce1140e73d68c45fd1
ssslavev <s.s.slavev@hotmail.com>
2019-03-06 22:23:29 +0200

    fix

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname+'/dist/my-social-net'));
+app.use(express.static(__dirname+'/dist/frontend'));
 
 app.get('/*', function(req,res) {
     
-    res.sendFile(path.join(__dirname + '/dist/my-social-net/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/frontend/index.html'));
     });
     
 
