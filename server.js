98485ab0690536c8eeec8c5f369f5568fa0ec19c
ssslavev <s.s.slavev@hotmail.com>
2019-03-06 21:33:13 +0200

    fix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static('dist'));
 
 app.get('/*', function(req,res) {
     
